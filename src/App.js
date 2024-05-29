62762de313599eb0a3f67ff89c19df483cbd1e46
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-05-29 13:41:34 -0500

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// App is the main component
+
+// This is a Hook
+import { useState } from "react";
 
 function App() {
+  const [userName, setUserName] = useState("");
+
+  const handleInput = (event) => {
+    setUserName(event.target.value);
+  };
+
+  let title = "Introduction to React";
+
+  let message = (
+    <h3 className="mt-20">
+      Welcome to React, <span>{userName}</span>
+    </h3>
+  );
+
+  const sectionTitle = <h2 className="mt-20">Using JSX</h2>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main>
+      <div className="container">
+        {/* Comments on JSX */}
+        <h2>{title}</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is a popular JavaScript library for building user interfaces,
+          particularly for single-page applications. It allows developers to
+          create large web applications that can change data, without reloading
+          the page. One of its key features is the ability to break down complex
+          UIs into simpler components.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {sectionTitle}
+        {message}
+        <input
+          type="text"
+          className="input"
+          placeholder="Username"
+          onChange={handleInput}
+        />
+      </div>
+    </main>
   );
 }
 
